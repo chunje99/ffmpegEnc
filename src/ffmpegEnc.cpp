65a05eba3ff817454ce1823a218ceda10d6b3874
Marco Uno <marco.uno@kakaocorp.com>
2019-01-29 13:40:17 +0900

    modify

--- src/ffmpegEnc.cpp
+++ src/ffmpegEnc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #define BUFF_SIZE 128
 
@@ -11,7 +12,6 @@ void Usage(){
 }
 
 int main(int argc, char*argv[]){
-    std::cout << "Hello World!" << std::endl;
     if(argc != 4)
         Usage();
 
@@ -19,6 +19,7 @@ int main(int argc, char*argv[]){
     std::string outFile = argv[2];
     std::string preset = argv[3];
     std::string options;
+    std::string processFile = outFile + ".process";
     if(preset == "FHD")
         options = " -s 1920x1080 -b:v 4M -b:a 512k ";
     else if(preset == "HD")
@@ -38,31 +39,45 @@ int main(int argc, char*argv[]){
         std::cerr << "popen Error" << std::endl;
         return -1;
     }
+    //open process file
+    std::ofstream of(processFile.c_str(), std::ios::trunc);
+    if(!of.is_open()){
+        std::cerr << "file open Error" << processFile << std::endl;
+        return -2;
+    }
+    of << "0" << std::endl;
     std::string buffStr, lineStr;
     char buff[BUFF_SIZE+1];
-    //while( fgets( buff, BUFF_SIZE, fp) ){
-    //while( fgets( buff, BUFF_SIZE, fp) ){
     int c = 0;
-    char h;
+    char t;
+    int duration, process;
+    size_t pos;
+    int h,m,s,ms;
     while(1){
-        //c = fread(buff, BUFF_SIZE,1, fp);
-        h = fgetc(fp);
-        if(h == EOF) break;
-        //if(h == '\n' || h == 4 || h == 13){
-        if(h == '\n' || h == '\r') {
-            if(lineStr.find("Duration:") != std::string::npos)
-                std::cout << lineStr << std::endl;
-            else if(lineStr.find("time=") != std::string::npos){
-                std::cout << lineStr << std::endl;
+        t = fgetc(fp);
+        if(t == EOF) break;
+        if(t == '\n' || t == '\r') {
+            if((pos = lineStr.find("Duration:")) != std::string::npos){
+                h=m=s=ms=0;
+                std::string du = lineStr.substr(pos+10,11);
+                sscanf((char*)du.data(), "%02d:%02d:%02d.%02d", &h, &m, &s, &ms);
+                duration = s + m*60 +  h*60*60;
+            } else if((pos = lineStr.find("time=")) != std::string::npos){
+                if(duration == 0) continue;
+                std::string pro = lineStr.substr(pos+5,11);
+                h=m=s=ms=0;
+                sscanf((char*)pro.data(), "%02d:%02d:%02d.%02d", &h, &m, &s, &ms);
+                process = s + m*60 +  h*60*60;
+                of << process*100/duration << std::endl;
            }
             lineStr = "";
         } else {
-            lineStr.push_back(h);
-            //std::cout << lineStr << std::endl;
-            //std::cout << (int)h << std::endl;
+            lineStr.push_back(t);
         } 
 
     }
+    of << 100 << std::endl;
+    of.close();
     pclose(fp);
     return 0;
 }
